547d9b317cb504dda5107601f96e856c5fac2859
iuhairways <103064103+iuhairways@users.noreply.github.com>
2022-12-26 18:28:25 +0000

    Update beta.js

--- beta/beta.js
+++ beta/beta.js
@@ -49,7 +49,7 @@ updateMultiplayer();
 }, 5000)
 document.querySelectorAll('[data-aircraft]').forEach(function(e){
    var elemName = e.outerText;
-    if (elemName.includes("Airbus a320neo (Iberia) (by Spice_9)") || elemName.includes("Boeing 737 Max 8 (TUI) (by Spice_9)") || elemName.includes("Boeing 787-10 Dreamliner (Etihad) (by Spice_9)") || elemName.includes("Airbus A319 (Finnair)  (by GT-VRA)")) {
+    if (elemName.includes("Airbus A380") || elemName.includes("Boeing 737-700")) {
        e.innerHTML = e.innerHTML + " [Multiliveries Frame]"
     }
 });
